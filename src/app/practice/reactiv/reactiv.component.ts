ce0d70bd08515657c10bf1a4b09053303bd4bec6
Manimaran <manimaracbe@gmail.com>
2020-06-29 22:19:17 +0530

    custom validation

--- src/app/practice/reactiv/reactiv.component.ts
+++ src/app/practice/reactiv/reactiv.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-reactiv',
@@ -12,19 +12,34 @@ export class ReactivComponent implements OnInit {
   isSubmitted = false;
   userData : Object = {};
   signupForm : FormGroup;
+  registeredEmail = ['abc@abc.abc', 'def@def.def'];
   constructor() { }
 
   ngOnInit(): void {
     this.signupForm = new FormGroup({
-      'email' : new FormControl('initial Value',[Validators.required, Validators.email ],),
+      'email' : new FormControl('initial Value',[Validators.required, Validators.email, this.checkExistingEmail.bind(this) ],),
       'pass' : new FormControl(),
       'learn' : new FormControl(),
-
+      'hobbies' : new FormArray([])
     });
   }
   onSubmit(){
     console.log(this.signupForm);
+  }
 
+  getAllControls(){
+    return (this.signupForm.get('hobbies') as FormArray).controls;
+  }
+  onAddHobbies(){
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  checkExistingEmail(control: FormControl) : {[s:string]: boolean} {
+    if(this.registeredEmail.indexOf(control.value) != -1)
+    {
+      return {'registered' : true};
+    }
   }
 
 }
