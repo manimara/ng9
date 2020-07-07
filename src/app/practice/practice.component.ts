36561cd71e038d034c123b5f9ad604e968ac691e
Manimaran <manimaracbe@gmail.com>
2020-07-07 15:14:36 +0530

    practice for observer, observables

--- src/app/practice/practice.component.ts
+++ src/app/practice/practice.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import {NgForm} from '@angular/forms';
+import { Observable } from 'rxjs';
+import { error } from 'protractor';
 
 @Component({
   selector: 'app-practice',
@@ -7,16 +9,42 @@ import {NgForm} from '@angular/forms';
   styleUrls: ['./practice.component.scss']
 })
 export class PracticeComponent implements OnInit {
+  count: number = 0;
+  count$ : Observable<number> = Observable.create((observer) => {
+    let count = 0;
+    setInterval(() => {
+      observer.next(count);
+      if(count == 3){
+        observer.error(new Error('count should be less then 5'));
+      }
+
+      if(count == 4){
+        observer.complete();
+      }
+      count ++;
+    }, 1000);
+  });
 
   @ViewChild('f') signupForm : NgForm;
   userData : Object = {};
   learningOptions = ['Basic', 'Advanced', 'Pro']
   defaultLearningOption = 'Advanced';
 
+
   constructor() { }
 
   ngOnInit(): void {
+    this.count$.subscribe( a => {
+      this.count = a;
+      console.log(a);
+    }, (e) => {
+      console.log(e);
+      console.log(e.message);
+    }, () => {
+      console.log(`Observable is completed`);
+    } )
 
   }
+
 
 }
